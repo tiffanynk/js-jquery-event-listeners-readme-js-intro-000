90b83e0217069028df2ff4ed03b63861841c8f15
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 21:52:29 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').addEventListener("keydown", event => {
+  $('#typing').addEventListener("keydown", event => {
     if (event.keyCode === 71) {
       return ('You pressed G!');
     }
