0eba6c0f3c7f6487483280a8797ae553c1ac03ae
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 22:14:42 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,6 +18,11 @@ function pressIt() {
   })
 }
 
+function.submitIt() {
+  $('form').on('submit', function => {
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 getIt();
